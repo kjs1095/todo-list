17f84f72ebbf13edf87583809a48cec9e84ea1fc
Josh Kao <kjs1095@gmail.com>
2019-04-21 15:40:27 +0800

    test: extend FT look for second row

--- functional_tests.py
+++ functional_tests.py
@@ -45,6 +45,14 @@ class NewVisitorTest(unittest.TestCase):
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn("2: Use peacock feathers to make a fly", [row.text for row in rows])
+
         self.fail("Finish th test!")
 
         # The page updates again, and now shows both items on her list
