4d2f1ff4e0c61870a7b5ad1abe86bb6334359420
Josh Kao <kjs1095@gmail.com>
2019-04-21 16:45:38 +0800

    test(superlists): add test that home page displays all list items

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,11 +1,21 @@
 from django.test import TestCase
 from lists.models import Item
 
+
 class HomePageTest(TestCase):
 
     def test_home_page_template(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
+
+    def test_display_all_list_items(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+
+        response = self.client.get("/")
+
+        self.assertIn("itemey 1", response.content.decode())
+        self.assertIn("itemey 2", response.content.decode())
 
     def test_can_save_a_POST_request(self):
         self.client.post("/", data={"item_text": "A new list item"})
@@ -17,7 +27,7 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post("/", data={"item_text": "A new list item"})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response["location"], "/") 
+        self.assertEqual(response["location"], "/")
 
     def test_only_saves_items_when_necessary(self):
         self.client.get("/")
